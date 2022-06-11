a849a768ba6f997e6d0d2022cd74077ae174848c
Ryan Kim <ryankim0709@gmail.com>
2022-06-10 21:28:52 -0700

    CSES Stick Lengths

---
+++ cpp/programs/Silver/CSES/Stick_Lengths/lengths.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    ll median;
+
+    if(n % 2 == 1) {
+        median = arr[n / 2];
+    } else {
+        median = (arr[n / 2 - 1] + arr[n / 2]) / 2;
+    }
+
+    ll ans = 0;
+    for(ll i:arr) {
+        ans += abs(i - median);
+    }
+
+    cout << ans;
+}
