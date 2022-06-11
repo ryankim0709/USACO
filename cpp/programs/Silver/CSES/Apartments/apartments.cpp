7fa6ec13e7ac2f09a953ee1a3e0836e38f976e3c
Ryan Kim <ryankim0709@gmail.com>
2022-06-10 22:17:10 -0700

    CSES Apartments

---
+++ cpp/programs/Silver/CSES/Apartments/apartments.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    int sizes[n];
+    int available[m];
+
+    for (int i = 0; i < n; i++) {
+        cin >> sizes[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> available[i];
+    }
+
+    sort(sizes, sizes + n);
+    sort(available, available + m);
+    int ans = 0;
+
+    int roomInd = m - 1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int curr = sizes[i];
+        bool found = false;
+        while(!found) {
+            if(roomInd == -1) {
+                found = true;
+                break;
+            }
+            int possibleRoom = available[roomInd];
+            if(possibleRoom <= curr + k && possibleRoom >= curr - k) {
+                ans++;
+                roomInd--;
+                found = true;
+            } else if(possibleRoom > curr + k) {
+                roomInd--;
+            } else if(possibleRoom < curr - k) {
+                found = true;
+            }
+        }
+    }
+    cout << ans;
+}
