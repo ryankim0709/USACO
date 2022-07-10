5e73fc826a47187477ac8bb711475e6be498a260
Ryan Kim <ryankim0709@gmail.com>
2022-07-09 20:55:22 -0700

    Competitive Programming Handbook Ch9

--- cpp/programs/Handbook/Ch9-Range_Queries/1-static_array_queries/README.md
+++ cpp/programs/Handbook/Ch9-Range_Queries/1-static_array_queries/README.md
@@ -1,1 +1,15 @@
 # Static array queries
+
+We will be working with the array `[1,3,4,8,6,1,4,2]`
+
+## Sum queries (1-D)
+
+For sum queries, we will be creating a prefix array. A prefix array will contain the sum of all preceding elements in a list. The length of the prefix array will be `n` and the starting element will be the first element of `arr`. We can construct a prefix array defining `prefix[i] = prefix[i - 1] + arr[i];`. In our example, our prefix array would be `[1,4,12,18,19,23,25]`.
+
+Now, we can tell that our `prefix[i]` is the sum of all elements from `arr[0]` to `arr[i - 1]`. Using this information, we can find the sum from `arr[a]` to `arr[b]`. Since `prefix[i]` is the sum of all elements from `0` to `i - 1` in our original array, we know that `sum(a, b) = prefix[b] - prefix[a-1]`. I would suggest drawing out an example and it will make clear sense.
+
+## Sum queries (2-D)
+
+Similar to 1-D queries, we need to make a 2-D `prefix` matrix. The formula for this 2-D prefix matrix is, `prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i - 1][j-1] + arr[i][j]`. Again, drawing will really help understand this concept.
+
+Now if we want to find the sum of values from `(x1, y1)` to `(x2, y2)` where `(x1, y1)` is the upper left and `(x2, y2)` is the lower left, our formula is `sum(x1, y1, x2, y2) = prefix[x2][y2] - prefix[x2][y1 - 1] - prefix[x1][y2-1] + prefix[x1 - 1][y1 - 1];`
--- cpp/programs/Handbook/Ch9-Range_Queries/static_range.cpp
+++ cpp/programs/Handbook/Ch9-Range_Queries/static_range.cpp
@@ -5,8 +5,12 @@ using namespace std;
 int main() {
 
     // Static range queries
+    
+    // Static array sums
+
+    // 1-D array
     int n = 8;
-    int arr[n] = {1, 3, 4, 8, 6, 1, 4, 2};
+    int arr[] = {1, 3, 4, 8, 6, 1, 4, 2};
     int prefix[n + 1];
 
     for (int i = 1; i < n + 1; i++) 
@@ -14,5 +18,34 @@ int main() {
     
     // sum(a, b) = sum(0,b) - sum(0,a-1)
     // Makes sense because you are subtracting the portion you don't want (0, a-1) from the whole, (0,b)
-    
+
+    int sum58 = prefix[8] - prefix[5 - 1]; // 6 + 1 + 4 + 2 = 13
+    cout << "1-D prefix " << sum58 << "\n";
+
+    // 2-D array
+    n = 4;
+    int arr2d[4][4] = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
+    int prefix2d[n+1][n+1]; // Generate sums from (0,0) to (x,y)
+
+    // preifx[row][col] = prefix[row - 1][col] + prefix[row][col - 1] - prefix[row - 1][col - 1] + arr[row - 1][col - 1]
+    for (int row = 0; row < n + 1; row ++) {
+        for (int col = 0; col < n + 1; col ++) {
+            if(row == 0 || col == 0) {
+                prefix2d[row][col] = 0;
+                continue;
+                
+            }
+            prefix2d[row][col] = prefix2d[row - 1][col] + prefix2d[row][col - 1] - prefix2d[row - 1][col - 1] + arr2d[row - 1][col - 1];
+        }
+    }
+
+    // Sum from (1,2) to (3,3);
+    int fx = 1;
+    int fy = 2;
+    int sx = 3;
+    int sy = 3;
+    int sum1223 = prefix2d[sx][sy] - prefix2d[sx][fy - 1] - prefix2d[fx - 1][sy] + prefix2d[fx - 1][fy - 1];
+    cout << "2-D prefix " << sum1223 << "\n";
+
+    // Minimum queries
 }
